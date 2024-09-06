37341812aeed57d9782c9d4446e5a7f7da80d198
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-09-06 16:29:00 -0300

    Add show/hide buttons and friend selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,135 @@ import { useState } from 'react';
 
 export default function App() {
   const [friends, setFriends] = useState([]);
+  const [showButton, setShowButton] = useState(false);
+  const friendSelected = friends.find((friend) => friend.selected) || null;
 
   function handleNewFriend(friend) {
     setFriends((friends) => [...friends, friend]);
+    toggleShowButton();
+  }
+
+  function handleFriendSelect(id) {
+    setFriends((friends) => {
+      const updatedFriend = friends.find((friend) => friend.id === id);
+      // Se o amigo já estiver selecionado, desativa todos
+      if (updatedFriend.selected) {
+        return friends.map((friend) => ({ ...friend, selected: false }));
+      }
+      // Caso contrário, ativa o amigo selecionado e desativa os demais
+      return friends.map((friend) => ({
+        ...friend,
+        selected: friend.id === updatedFriend.id,
+      }));
+    });
+  }
+
+  function toggleShowButton() {
+    setShowButton((showButton) => setShowButton(!showButton));
   }
 
   return (
-    <div>
-      <FriendList friends={friends} />
-      <AddFriend onAddFriend={(friend) => handleNewFriend(friend)} />
+    <div className="App">
+      <div className="user-and-split-view">
+        <FriendList
+          friends={friends}
+          onSelect={(id) => handleFriendSelect(id)}
+        />
+        {friendSelected === null ? '' : <SplitBill friend={friendSelected} />}
+      </div>
+      {!showButton && (
+        <ButtonAddClose text="Add Friend" onClick={toggleShowButton} />
+      )}
+      {showButton && (
+        <AddFriend onAddFriend={(friend) => handleNewFriend(friend)} />
+      )}
+      {showButton && <ButtonAddClose text="Close" onClick={toggleShowButton} />}
     </div>
+  );
+}
+
+function SplitBill({ friend }) {
+  const [bill, setBill] = useState(0);
+  const [yourExpense, setYourExpense] = useState(0);
+  const [otherExpense, setOtherExpense] = useState(0);
+  const [whoPays, setWhoPays] = useState('You');
+
+  return (
+    <div className="split-bill">
+      <h3>Split a bill with {friend.name}</h3>
+      <form>
+        <div>
+          <label>💵 Bill value</label>
+          <input
+            type="number"
+            value={bill}
+            onChange={(e) => setBill(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <label>🧍🏻‍♂️ Your expense</label>
+          <input
+            type="number"
+            value={yourExpense}
+            onChange={(e) => setYourExpense(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <label>👨🏻‍🤝‍👩 {friend.name} expense</label>
+          <input
+            type="number"
+            value={otherExpense}
+            onChange={(e) => setOtherExpense(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <label>Who is paying the bill</label>
+          <select value={whoPays} onChange={(e) => setWhoPays(e.target.value)}>
+            <option value="You"></option>
+            <option value={friend.name}></option>
+          </select>
+        </div>
+      </form>
+      <button type="submit">Split bill</button>
+    </div>
+  );
+}
+
+function FriendList({ friends, onSelect }) {
+  return (
+    <table className="tb-friend-list">
+      <tbody>
+        {friends.map((friend) => (
+          <tr key={friend.id}>
+            <Friend
+              id={friend.id}
+              name={friend.name}
+              img_url={friend.img_url}
+              balance={friend.balance}
+              selected={friend.selected}
+              onSelect={onSelect}
+            />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function Friend({ id, name, img_url, balance, selected, onSelect }) {
+  return (
+    <td className="friend">
+      <img className="friend-image" src={img_url} alt="batman picture" />
+      <div className="friend-txt">
+        <p className="friend-name">{name}</p>
+        <p>
+          {name} owes you ${balance}
+        </p>
+      </div>
+      <button className="friend-select-btn" onClick={() => onSelect(id)}>
+        {selected ? 'Close' : 'Select'}
+      </button>
+    </td>
   );
 }
 
@@ -55,41 +174,19 @@ function AddFriend({ onAddFriend }) {
           onChange={(e) => setImgUrl(e.target.value)}
         />
       </div>
-      <button type="submit">Add</button>
+      <button disabled={!name || !img_url} type="submit">
+        Add
+      </button>
     </form>
   );
 }
 
-function FriendList({ friends }) {
+function ButtonAddClose({ text, onClick }) {
   return (
-    <table className="tb-friend-list">
-      <tbody>
-        {friends.map((friend) => (
-          <tr key={friend.id}>
-            <Friend
-              name={friend.name}
-              img_url={friend.img_url}
-              balance={friend.balance}
-              selected={friend.selected}
-            />
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
-
-function Friend({ name, img_url, balance, selected }) {
-  return (
-    <td className="friend">
-      <img className="friend-image" src={img_url} alt="batman picture" />
-      <div className="friend-txt">
-        <p className="friend-name">{name}</p>
-        <p>
-          {name} owes you ${balance}
-        </p>
-      </div>
-      <button className="friend-btn">{selected ? 'Close' : 'Select'}</button>
-    </td>
+    <div>
+      <button className="btn-add-close-friend" onClick={onClick}>
+        {text}
+      </button>
+    </div>
   );
 }
